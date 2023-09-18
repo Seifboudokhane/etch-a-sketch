36dcc7647d3b6568a53edf2b0687018fa5ba0864
Seif Boudokhane <113269052+Seifboudokhane@users.noreply.github.com>
2023-09-18 20:42:22 +0100

    Make 16*16 grid with js and Color it on click/drag

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+for (let i =0; i<256;i++){
+    const gridElement= document.createElement("div");
+    gridElement.classList.add("gridElement");
+    const container= document.getElementById("container");
+    container.appendChild(gridElement);
+}
+const gridElements = document.querySelectorAll(".gridElement");
 
+/* Flag to know if mouse is down on table */
+let MDOWN = false;
+
+
+function cellEnter(){
+    if (MDOWN) {
+        this.classList.add("color");
+    }
+}
+
+gridElements.forEach(function(e){
+    e.addEventListener("mousedown",()=>e.classList.add("color"));
+    e.addEventListener("mousedown",()=>MDOWN = !MDOWN);
+    e.addEventListener("mouseup",()=>MDOWN = !MDOWN);
+    e.addEventListener("mouseenter",cellEnter);
+    e.addEventListener("dragstart",event=> event.preventDefault());
+});
+
+// gridElements.forEach(function(e){
+//     e.addEventListener("dragover",()=>e.classList.add("color"))
+//     e.addEventListener("mousedown",()=>e.classList.add("color"))
+// });
